068e770fd728d9aa36d024b23efcf6909bdaf545
Efte Hassan <eftehassanpp@gmail.com>
2023-06-01 02:14:24 -0400

    form

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -3,28 +3,67 @@ import React from 'react';
 export default class Form extends React.Component {
   state = {
     title: '',
+    text: '',
+    library: 'React',
+    isAwesome: true,
   };
 
   handleChange = (event) => {
-    this.setState({
-      title: event.target.value,
-    });
+    if (event.target.type === 'text') {
+      this.setState({
+        title: event.target.value,
+      });
+    } else if (event.target.type === 'textarea') {
+      this.setState({
+        text: event.target.value,
+      });
+    } else if (event.target.type === 'select-one') {
+      this.setState({
+        library: event.target.value,
+      });
+    } else if (event.target.type === 'checkbox') {
+      this.setState({
+        isAwesome: event.target.checked,
+      });
+    }
+  };
+  submithandler = (event) => {
+    const { title, text, library, isAwesome } = this.state;
+    event.preventDefault();
+    console.log(title, text, library, isAwesome);
   };
   render() {
-    let { title } = this.state;
+    const { title, text, library, isAwesome } = this.state;
     return (
       <div>
-        <form>
+        <form onSubmit={this.submithandler}>
           <input
-            action=""
+            name="title"
             type="text"
             placeholder="Enter Title"
-            value={this.title}
+            value={title}
             onChange={this.handleChange}
           />
           <br />
           <br />
-          <textarea name="textArea"></textarea>
+          <textarea
+            name="text"
+            onChange={this.handleChange}
+            value={text}
+          ></textarea>
+          <br />
+          <select name="library" value={library} onChange={this.handleChange}>
+            <option value="React">React</option>
+            <option value="Angular">Angular</option>
+          </select>
+          <br />
+          <input
+            type="checkbox"
+            checked={isAwesome}
+            onChange={this.handleChange}
+          />
+          <br />
+          <input type="submit" value="Submit" />
         </form>
       </div>
     );
